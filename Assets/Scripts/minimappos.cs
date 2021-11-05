a39b289ff3e24285447de3148539ae668cd049c4
avichai1221 <57321080+avichai1221@users.noreply.github.com>
2021-11-05 16:15:09 +0200

    fix code

--- Assets/Scripts/minimappos.cs
+++ Assets/Scripts/minimappos.cs
@@ -1,24 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.IK;
 using UnityEngine;
 
 public class minimappos : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
 
+   //אנחנו לא רוצים שתזוזת המצלמה הקטנה תחרוג מגבולות המשחק
+   //בגלל שהמצלמה תלויה במיקום השחקן והשחקן מגיע לנקודה שיכולה לגרום למצלמה לצאת
+   //הגדרנו קבוע עבור מיקום המצלמה הקטנה שישמש כגבול
     public Transform player;
-
+    private float LimitForMiniMap = 5.835996f;
     void FixedUpdate()
     {
 
         Vector3 newpos = player.position;
         newpos.y = transform.position.y;
         newpos.z = -10;
-        if(newpos.x > 0 && newpos.x <= 5.835996)
+        if(newpos.x > 0 && newpos.x <= LimitForMiniMap) 
         {
             transform.position = newpos;
         }
-        if(newpos.x < 0 && newpos.x >= -5.835996)
+        if(newpos.x < 0 && newpos.x >= -LimitForMiniMap)
         {
             transform.position = newpos;
         }
